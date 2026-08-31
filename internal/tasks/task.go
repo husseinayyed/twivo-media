36e15f147afc0552dafc5dd9d3e50509b7d6f21e
husseinayyed <honor9x.hasson@gmail.com>
2026-08-31 20:41:05 +0400

    feat(tasks): replace gob with sonic for faster JSON serialization

--- internal/tasks/task.go
+++ internal/tasks/task.go
@@ -1,34 +1,27 @@
 package tasks
 
 import (
-	"bytes"
-	"encoding/gob"
+	"github.com/bytedance/sonic"
 	"fmt"
 	"github.com/hibiken/asynq"
 )
 
 type UploadPayload struct {
-	UserID   string
-	TweetID  string
-	FileUUID string
-	FileType string
-	Width string
-	Height string
+    UserID   string `json:"user_id"`
+    TweetID  string `json:"tweet_id"`
+    FileUUID string `json:"file_uuid"`
+    FileType string `json:"file_type"`
+    Width    string `json:"width"`
+    Height   string `json:"height"`
 }
 
 func (p *UploadPayload) Serialize() ([]byte, error) {
-	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(p); err != nil {
-		return nil, fmt.Errorf("failed to encode payload: %v", err)
-	}
-	return buf.Bytes(), nil
+    return sonic.Marshal(p)
 }
 
 func (p *UploadPayload) Deserialize(data []byte) error {
-	buf := bytes.NewBuffer(data)
-	return gob.NewDecoder(buf).Decode(p)
+    return sonic.Unmarshal(data, p)
 }
-
 func ScheduleUploadTask(ac *asynq.Client,data UploadPayload) error {
 	payload, err := data.Serialize()
 	if err != nil {
