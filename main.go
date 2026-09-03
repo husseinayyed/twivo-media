d449dbb4de3923e80ec59055e78f37b905e4440a
husseinayyed <honor9x.hasson@gmail.com>
2026-09-04 00:27:53 +0400

    main: initialize image cache, remove unused import (reorder imports)

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	_ "image/jpeg"
 	_ "image/png"
+
 	pprof "github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
-	"github.com/husseinayyed/twivo-media/internal/middleware"
+	"github.com/husseinayyed/twivo-media/internal/cache"
 	"github.com/husseinayyed/twivo-media/internal/handler"
+	"github.com/husseinayyed/twivo-media/internal/middleware"
 	_ "golang.org/x/image/webp"
 )
 
@@ -17,6 +19,7 @@ func main() {
 	router := gin.Default()
 	pprof.Register(router) // Register pprof routes for profiling and debugging
 	port := "8020"
+	cache.InitCache() // Initialize the LRU cache for storing image checksums
 	handler.InitWorker() // Initialize the worker for handling background tasks
 	handler.InitImageCache() // Initialize the image cache for storing image metadata
 	
